ee575988a871e40320baac2216d6b1c0861e6351
wszpfwszpf <31006237+wszpfwszpf@users.noreply.github.com>
2019-07-01 16:48:37 +0800

    Update 1001.c

--- PATBasic/1001.c
+++ PATBasic/1001.c
@@ -1,2 +1,19 @@
-
-
+#include <stdio.h>
+int main()
+{
+	int number ;
+	scanf("%d",&number);
+	int times = 0;
+	for (;number != 1;)
+	{
+		if (number%2 == 0)
+			number = number/2;
+		else if (number%2 == 1)
+		{
+			number = (3 * number + 1) / 2;
+		}
+		times++;	
+	}
+	printf("%d",times);
+	return 0;
+}
